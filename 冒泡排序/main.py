ea46b4580c484d63090fe8cd36fd897bed5ff22f
chenyk <chenyk1219@gmail.com>
2025-03-16 15:46:48 +0800

    1

--- 冒泡排序/main.py
+++ 冒泡排序/main.py
@@ -1,1 +1,10 @@
 def bubbleSort(alist):
+    for passnum in range(len(alist) - 1, 0, -1):
+        for i in range(passnum):
+            if alist[i] > alist[i + 1]:
+                alist[i], alist[i + 1] = alist[i + 1], alist[i]
+
+
+alist = [21, 56, 89, 12, 67, 31, 34, 56, 1, 4, 3, 2]
+bubbleSort(alist)
+print(alist)
