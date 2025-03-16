6b9adf2e710a8e4e7e536356ee56ee5683abaf56
chenyk <chenyk1219@gmail.com>
2025-03-16 20:01:21 +0800

    1

---
+++ 树/表达式解析.py
@@ -0,0 +1,87 @@
+from stack import Stack
+import operator
+
+
+class BinaryTree:
+    def __init__(self, rootObj):
+        self.key = rootObj
+        self.leftChild = None
+        self.rightChild = None
+
+    def insertLeft(self, newNode):
+        if self.leftChild is None:
+            self.leftChild = BinaryTree(newNode)
+        else:
+            t = BinaryTree(newNode)
+            t.leftChild = self.leftChild
+            self.leftChild = t
+
+    def insertRight(self, newNode):
+        if self.rightChild is None:
+            self.rightChild = BinaryTree(newNode)
+        else:
+            t = BinaryTree(newNode)
+            t.rightChild = self.rightChild
+            self.rightChild = t
+
+    def getRootVal(self):
+        return self.key
+
+    def setRootVal(self, obj):
+        self.key = obj
+
+    def getRightChild(self):
+        return self.rightChild
+
+    def getLeftChild(self):
+        return self.leftChild
+
+
+def buildParseTree(fpexp: str):
+    fplist = fpexp.split()
+    pStack = Stack()
+    eTree = BinaryTree('')
+    pStack.push(eTree)
+    currentTree = eTree
+    for i in fplist:
+        print(i)
+        if i == '(':
+            currentTree.insertLeft('')
+            pStack.push(currentTree)
+            currentTree = currentTree.getLeftChild()
+        elif i not in ['+', '-', '*', '/', ')']:
+            currentTree.setRootVal(int(i))
+            parent = pStack.pop()
+            currentTree = parent
+        elif i in ['+', '-', '*', '/']:
+            currentTree.setRootVal(i)
+            currentTree.insertRight('')
+            pStack.push(currentTree)
+            currentTree = currentTree.getRightChild()
+        elif i == ')':
+            currentTree = pStack.pop()
+        else:
+            raise ValueError
+
+    return eTree
+
+
+def evaluate(node):
+    opers = {'+': operator.add, '-': operator.sub, '*': operator.mul, '/': operator.truediv}
+
+    leftC = node.getLeftChild()
+    rightC = node.getRightChild()
+
+    if leftC and rightC:
+        fn = opers[node.getRootVal()]
+        return fn(evaluate(leftC), evaluate(rightC))
+    else:
+        return node.getRootVal()
+
+
+s = "( ( 3 + 4 ) * 2 )"
+eTree = buildParseTree(s)
+s1 = evaluate(eTree)
+print(s1)
+# print(eTree.getRootVal())
+# print(eTree.getRightChild().getRightChild().getRootVal())
